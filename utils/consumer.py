eb50684885508aa6a6a43c73d4d67b946e5cc97f
abdul7235 <abdul7235@gmail.com>
2023-08-28 17:54:51 +0500

    latest commit

--- utils/consumer.py
+++ utils/consumer.py
@@ -6,28 +6,30 @@ import time
 import cv2
 import numpy as np
 import wave
+import threading
+from queue_message_count import message_count
 
 # RabbitMQ connection parameters
 RABBITMQ_HOST = 'localhost'
-QUEUE_NAME = 'uid_stream_data'
+#QUEUE_NAME = '1234_stream_data'
  
-# Maximum consecutive empty queue count before exiting
-MAX_EMPTY_COUNT = 60
+
 
 def save_image_frame(uid, timestamp, frame_data):
     frame_array = np.frombuffer(base64.b64decode(frame_data), dtype=np.uint8)
     frame = cv2.imdecode(frame_array, flags=cv2.IMREAD_COLOR)
-    cv2.imwrite(f'{uid}_{timestamp}.jpg', frame)
+    cv2.imwrite(f'data/{uid}_{timestamp}.jpg', frame)
 
 def save_audio(uid, timestamp, audio_data):
-    audio_path = f'{uid}_{timestamp}.wav'
+    audio_path = f'data/{uid}_{timestamp}.wav'
     with wave.open(audio_path, 'wb') as wav_file:
         wav_file.setnchannels(1)
         wav_file.setsampwidth(2)
         wav_file.setframerate(44100)
-        wav_file.writeframes(audio_data)
+        # Convert audio_data to bytes before writing
+        wav_file.writeframes(audio_data.encode())
 
-def callback(ch, method, properties, body):
+def callback(ch, method, properties, body, stop_flag, connection):
     data = json.loads(body)
     uid = data['uid']
     timestamp = data['timestamp']
@@ -39,18 +41,24 @@ def callback(ch, method, properties, body):
 
     print(f"Processed message: {uid}_{timestamp}")
 
-    callback.count = getattr(callback, 'count', 0) + 1
-    if callback.count >= MAX_EMPTY_COUNT:
-        print("Exiting program due to empty queue.")
+    if stop_flag[0] == True:
         ch.stop_consuming()
+        #connection.close()
+
 
 def subscriber(uid):
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
     channel = connection.channel()
-
+    stop_flag= [False]
+    QUEUE_NAME = f"{uid}_stream_data"
     channel.queue_declare(queue=QUEUE_NAME)
-
-    channel.basic_consume(queue=QUEUE_NAME, on_message_callback=callback, auto_ack=True)
+    thread_message_count = threading.Thread(target=message_count, args=(stop_flag, QUEUE_NAME))
+    thread_message_count.start()
+    callback_wrapper = lambda ch, method, properties, body: callback(ch, method, properties, body, stop_flag,connection)
+    channel.basic_consume(queue=QUEUE_NAME, on_message_callback=callback_wrapper, auto_ack=True)
 
     print("Waiting for messages. To exit press CTRL+C")
     channel.start_consuming()
+
+
+subscriber('123456')
--- utils/queue_message_count.py
+++ utils/queue_message_count.py
@@ -1,26 +1,28 @@
 import pika
 
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))  
+RABBITMQ_HOST = 'localhost'
 
 
-channel = connection.channel()
+def message_count(stop_flag, queue_name):
+    consecutive_false_count = 0
+    false_list = []
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
+    channel = connection.channel()
+    queue_info = channel.queue_declare(queue=queue_name, passive=True)
+    
+    while True:
+        message_count = queue_info.method.message_count
+        
+        if len(false_list) == 3000:
+            stop_flag[0] = True  
+            print(stop_flag[0])
+            break
 
+        if message_count == 0:
+            consecutive_false_count += 1
+            false_list.append(False)
+        else:
+            false_list = []
 
-queue_name = '123_audio'
-
-
-channel.queue_declare(queue=queue_name)
-
-
-queue_info = channel.queue_declare(queue=queue_name, passive=True)
-message_count = queue_info.method.message_count
-
-
-if message_count == 0:
-    print(f"The queue '{queue_name}' is empty.")
-else:
-    print(f"The queue '{queue_name}' contains {message_count} messages.")
-
-
-connection.close()
+    #connection.close()
